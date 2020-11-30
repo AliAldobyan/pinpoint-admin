15b00a077d4d1ce0d4e8832ada39a66b611a748d
Mohammed Alzaid <m.al-zaid@live.com>
2020-11-30 11:48:04 +0300

    create journey

--- src/components/Journey/JourneyCreate.js
+++ src/components/Journey/JourneyCreate.js
@@ -1,24 +1,81 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {connect} from "react-redux";
-import JourneyShipments from "./JourneyShipments";
-import JourneyMap from "./JourneyMap";
 import JourneyMapCreate from "./JourneyMapCreate";
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
+import {addJourney} from "../../redux/actions";
+import {InputText} from "primereact/inputtext";
+import moment from "moment";
 
-const JourneyCreate = ({ shipments }) => {
+const JourneyCreate = ({ shipments, createJourney }) => {
+
+    const [selectedShipments, setSelectedShipments] = useState([]);
+    const [travelTime, setTravelTime] = useState(0)
+    const [previousLength, setPreviousLength] = useState(0)
+
+    if (!shipments) return <h1>Loading...</h1>
+
     const filteredShipments = shipments.filter(shipment => shipment.status?.id === 1 || shipment.status?.id === 4)
-    const waitingShipments = filteredShipments.map(shipment => (
-        <JourneyShipments
-            key={shipment.id}
-            shipment={shipment}
-        />
-    ))
+
+    const handleClick = () => {
+        createJourney({packages_list: selectedShipments.map(shipment => shipment.id)})
+        setPreviousLength(0)
+        setTravelTime(0)
+        setSelectedShipments([])
+    }
+
+    const header = (
+        <div className="table-header">
+            <p>Travel Time: {travelTime}</p>
+            {/*{selectedShipments.length >= 1 && <button className="btn btn-primary" onClick={handleClick}>Create Journey</button>}*/}
+
+      {/*      <h5 className="p-m-0">Manage Shipments</h5>*/}
+      {/*      <span className="p-input-icon-left">*/}
+      {/*  <i className="pi pi-search" />*/}
+      {/*  <InputText*/}
+      {/*      type="search"*/}
+      {/*      onInput={(e) => {}}*/}
+      {/*      placeholder="Search..."*/}
+      {/*  />*/}
+      {/*</span>*/}
+        </div>
+    );
+    const dateBodyTemplate = (rowData) => {
+        return (
+            <span >
+        {moment(rowData.date_added).format("YYYY-MM-DD")}
+      </span>
+        );
+    };
     return (
-        <div>
-            {waitingShipments}
-            <JourneyMapCreate shipments={filteredShipments}/>
+        <div className="journey">
+                {/*<h1>Travel Time: {travelTime}</h1>*/}
+                {/*{selectedShipments.length > 1 && <button className="btn btn-primary" onClick={handleClick}>Create Journey</button>}*/}
+            <div>
+                <DataTable value={filteredShipments} header={header} selection={selectedShipments}
+                           onSelectionChange={e => setSelectedShipments(e.value)} selectionMode="multiple" dataKey="id" metaKeySelection={false}
+                           scrollable scrollHeight="38vh">
+                    <Column field="id" header="ID"/>
+                    <Column field="tracking_num" header="Tracking Number"/>
+                    <Column field="receiver_name" header="Receiver Name"/>
+                    <Column field="receiver_phone" header="Quantity"/>
+                    <Column field="date_added" header="Quantity" body={dateBodyTemplate} sortable/>
+                    <Column field="added_by.first_name" header="Quantity"/>
+                    <Column field="preferred_time.time" header="Quantity"/>
+                    <Column field="status.name" header="Quantity"/>
+                    {/*<Column field="date_added" header="Quantity"/>*/}
+                </DataTable>
+            </div>
+            <JourneyMapCreate shipments={filteredShipments}
+                              selected={selectedShipments} setSelected={setSelectedShipments}
+                              travelTime={travelTime} setTravelTime={setTravelTime}
+                              previousLength={previousLength} setPreviousLength={setPreviousLength}
+            />
+
         </div>
     );
 };
+
 
 const mapStateToProps = ({ journeys, shipments }) => ({
     journeys,
@@ -26,4 +83,10 @@ const mapStateToProps = ({ journeys, shipments }) => ({
     loading: !shipments,
 });
 
-export default connect(mapStateToProps)(JourneyCreate);
+const mapDispatchToProps = (dispatch) => {
+    return {
+        createJourney: (item) => dispatch(addJourney(item)),
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(JourneyCreate);
--- src/components/Journey/JourneyMapCreate.js
+++ src/components/Journey/JourneyMapCreate.js
@@ -1,28 +1,22 @@
-import React, { useState, useEffect } from 'react';
-import { GoogleMap, useLoadScript, Marker, InfoWindow, DistanceMatrixService } from "@react-google-maps/api";
+import React, { useEffect } from 'react';
+import { GoogleMap, useLoadScript, Marker, InfoWindow } from "@react-google-maps/api";
 import {connect} from "react-redux";
-import {addJourney} from "../../redux/actions";
-// import "@reach/combobox/styles.css";
 
 const libraries = ["places"];
 const mapContainerStyle = {
-    height: "100vh",
-    width: "100vw",
+    height: "50vh",
+    width: "100%",
 };
 const center = {
     lat: 24.711988,
     lng: 46.663406,
 };
-const JourneyMap = ({ shipments, createJourney }) => {
+const JourneyMap = ({ shipments, selected, setSelected, travelTime, setTravelTime, previousLength, setPreviousLength }) => {
 
     const { isLoaded, loadError } = useLoadScript({
         googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
         libraries,
     });
-
-    const [selected, setSelected] = useState([])
-    const [travelTime, setTravelTime] = useState(0)
-    const [previousLength, setPreviousLength] = useState(0)
 
     useEffect(() => {
         try {
@@ -32,16 +26,14 @@ const JourneyMap = ({ shipments, createJourney }) => {
                 if (selected.length > 1) {
                     let newTravelTime = 0
                     for (let i = 1; i < selected.length; i++) {
-                        let originShipment = shipments.find(shipment => shipment.id === selected[i - 1])
-                        let destinationShipment = shipments.find(shipment => shipment.id === selected[i])
+                        let originShipment = selected[i - 1]
+                        let destinationShipment = selected[i]
                         let origin = new window.google.maps.LatLng(Number(originShipment.latitude), Number(originShipment.longitude));
-                        let destination = new window.google.maps.LatLng(Number(destinationShipment.latitude), Number(originShipment.longitude))
+                        let destination = new window.google.maps.LatLng(Number(destinationShipment.latitude), Number(destinationShipment.longitude))
                         const callback = (response) => {
-                            console.log(response)
                             let travel = Math.ceil(response.rows[0].elements[0].duration.value / 60) + 5
                             newTravelTime += travel
                             setTravelTime(newTravelTime)
-                            console.log(travelTime)
                         }
                         let service = new window.google.maps.DistanceMatrixService();
                         service.getDistanceMatrix(
@@ -57,15 +49,13 @@ const JourneyMap = ({ shipments, createJourney }) => {
             else {
                 setPreviousLength(selected.length)
                 if (selected.length > 1) {
-                    let originShipment = shipments.find(shipment => shipment.id === selected[selected.length - 2])
-                    let destinationShipment = shipments.find(shipment => shipment.id === selected[selected.length - 1])
+                    let originShipment = selected[selected.length - 2]
+                    let destinationShipment = selected[selected.length - 1]
                     let origin = new window.google.maps.LatLng(Number(originShipment.latitude), Number(originShipment.longitude));
-                    let destination = new window.google.maps.LatLng(Number(destinationShipment.latitude), Number(originShipment.longitude))
+                    let destination = new window.google.maps.LatLng(Number(destinationShipment.latitude), Number(destinationShipment.longitude))
                     const callback = (response) => {
-                        console.log(response)
                         let travel = Math.ceil(response.rows[0].elements[0].duration.value / 60) + 5
                         setTravelTime(travelTime + travel)
-                        console.log(travelTime)
                     }
                     let service = new window.google.maps.DistanceMatrixService();
                     service.getDistanceMatrix(
@@ -87,32 +77,23 @@ const JourneyMap = ({ shipments, createJourney }) => {
     if (!isLoaded) return "Loading...";
     if (!shipments) return <h1> LOADING...</h1>;
 
-    const handleClick = () => {
-        createJourney({packages_list: selected})
-        setPreviousLength(0)
-        setTravelTime(0)
-        setSelected([])
-    }
-
     const markers = shipments?.map(shipment => (
         <Marker
             key={shipment.id}
             position={{lat: Number(shipment.latitude), lng: Number(shipment.longitude)}}
             onClick={() => {
-                setSelected(selected.includes(shipment.id) ? selected.filter(pin => pin !== shipment.id) : [...selected, shipment.id]);
+                setSelected(selected.some(item => item.id === shipment.id) ? selected.filter(pin => pin.id !== shipment.id) : [...selected, shipment]);
             }}
-            icon={selected.includes(shipment.id)?{
+            icon={selected.some(item => item.id === shipment.id) ? {
                 url: `http://maps.google.com/mapfiles/ms/icons/green-dot.png`,
             }:{
                 url: `http://maps.google.com/mapfiles/ms/icons/red-dot.png`,
             }}
         />
     ))
-    console.log(selected)
+
     return (
         <div>
-            <h1>Travel Time: {travelTime}</h1>
-            {selected.length > 1 && <button className="btn btn-primary" onClick={handleClick}>Create Journey</button>}
             <GoogleMap
                 id="map"
                 mapContainerStyle={mapContainerStyle}
@@ -129,10 +110,4 @@ const mapStateToProps = ({ journeys }) => ({
     journeys,
 });
 
-const mapDispatchToProps = (dispatch) => {
-    return {
-        createJourney: (item) => dispatch(addJourney(item)),
-    };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(JourneyMap);
+export default connect(mapStateToProps)(JourneyMap);
